333a0990c7e23814c833dcdbf8c47696cef9c1f4
Hoon Lee <yl.hoony@gmail.com>
2018-01-19 23:32:15 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+
+  # resources :register, only: [:index, :show, :new]
+  get '/register', to: 'users#new', as: 'register'
 end
