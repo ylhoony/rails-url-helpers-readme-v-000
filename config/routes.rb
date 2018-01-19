79dbeb81f7aa1e19bead56a70120d54d11ab6c28
Hoon Lee <yl.hoony@gmail.com>
2018-01-19 23:30:02 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
 
-  resources :register, only: [:index, :show, :new]
-  # get '/register', to: 'users#new'
+  # resources :register, only: [:index, :show, :new]
+  get '/register', to: 'users#new'
 end
